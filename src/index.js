129c78b74540ff76cb50ca790bc30bc2891a6467
Vasja Hajnas <129309015+VasjaHajnas@users.noreply.github.com>
2023-08-30 18:32:07 +0300

    homework-JS-10-finish2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import Notiflix from 'notiflix';
 
 const loader = document.querySelector('.loader');
@@ -6,22 +6,34 @@ const error = document.querySelector('.error');
 const breedSelect = document.querySelector('.breed-select');
 const catInfo = document.querySelector('.cat-info');
 
+// Initially, set the loader to 'block'
+loader.style.display = 'block';
+
 // Завантаження списку порід при завантаженні сторінки
 fetchBreeds()
   .then(breeds => {
+    // Hide the loader once data is loaded
     loader.style.display = 'none';
-    breeds.forEach(breed => {
-      const option = document.createElement('option');
-      option.value = breed.id;
-      option.textContent = breed.name;
-      breedSelect.appendChild(option);
-    });
+    try {
+      breeds.forEach(breed => {
+        const option = document.createElement('option');
+        option.value = breed.id;
+        option.textContent = breed.name;
+        breedSelect.appendChild(option);
+      });
+    } catch (error) {
+      console.error('Error while processing breeds:', error);
+      // Handle the error as needed, e.g., displaying an error message.
+    }
   })
-  .catch(() => {
+  .catch(error => {
     Notiflix.Notify.failure(
-      'Oops! Something went wrong! Try reloading the page!'
+      'Oops! Something went wrong while fetching breeds. Try reloading the page!'
     );
+    // Hide the loader in case of an error
     loader.style.display = 'none';
+    console.error('Error while fetching breeds:', error);
+    // Handle the error as needed.
   });
 
 // Обробник події для вибору породи
@@ -34,17 +46,15 @@ breedSelect.addEventListener('change', () => {
     fetchCatByBreed(selectedBreedId)
       .then(cat => {
         loader.style.display = 'none';
-        catInfo.style.display = 'flex'; // Змінити display на 'flex', щоб відображати текст поруч із зображенням
+        catInfo.style.display = 'flex';
         const catImage = document.createElement('img');
         catImage.src = cat.url;
 
-        catInfo.innerHTML = ''; // Очищаємо контейнер catInfo перед додаванням нових елементів
+        catInfo.innerHTML = '';
 
-        // Створюємо контейнери для текстової інформації
         const textContainer = document.createElement('div');
         textContainer.classList.add('text-container');
 
-        // Додаємо текстову інформацію до контейнера textContainer
         const catName = document.createElement('h1');
         catName.textContent = cat.breeds[0].name;
         const catDescription = document.createElement('p');
@@ -54,21 +64,21 @@ breedSelect.addEventListener('change', () => {
         const catTemperament = document.createElement('p');
         catTemperament.textContent = cat.breeds[0].temperament;
 
-        // Додаємо всі тексти до textContainer
         textContainer.appendChild(catName);
         textContainer.appendChild(catDescription);
         textContainer.appendChild(catTemperamentHeading);
         textContainer.appendChild(catTemperament);
 
-        // Додаємо зображення і textContainer до catInfo
         catInfo.appendChild(catImage);
         catInfo.appendChild(textContainer);
       })
-      .catch(() => {
+      .catch(error => {
         Notiflix.Notify.failure(
-          'Oops! Something went wrong! Try reloading the page!'
+          'Oops! Something went wrong while fetching cat information. Try reloading the page!'
         );
         loader.style.display = 'none';
+        console.error('Error while fetching cat information:', error);
+        // Handle the error as needed.
       });
   }
 });
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,26 +1,20 @@
 import axios from 'axios';
 
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
 axios.defaults.headers.common['x-api-key'] =
   'live_TJsaqooqBlwz6F47HLbtbnOdczo8WZEKMqv43Bb4ZWH9Q8KR3ILyJHWSu0md2mow';
 
 // Функція для отримання списку порід
 export async function fetchBreeds() {
-  try {
-    const response = await axios.get('https://api.thecatapi.com/v1/breeds');
-    return response.data;
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(`${BASE_URL}/breeds`);
+  return response.data;
 }
 
 // Функція для отримання інформації про кота за ідентифікатором породи
 export async function fetchCatByBreed(breedId) {
-  try {
-    const response = await axios.get(
-      `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`
-    );
-    return response.data[0];
-  } catch (error) {
-    throw error;
-  }
+  const response = await axios.get(
+    `${BASE_URL}/images/search?breed_ids=${breedId}`
+  );
+  return response.data[0];
 }
